562debbbdaea790e4f4f809f8e06cdd35f941c39
Andrexxx01 <kurniawan.andre50@gmail.com>
2026-03-15 21:30:36 +0700

    setup and testing backend-signup route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,14 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../lib/utils.js";
 
-export const signup = (req, res) => {
+export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
   try {
+    if (!fullName || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+    
     if (password.length < 6) {
         return res.status(400).json({ message: "Password must be at least 6 characters" })
     }
@@ -19,8 +24,25 @@ export const signup = (req, res) => {
         email,
         password:hashedPassword,
     })
-    
-  } catch (error)
+
+    if (newUser) {
+        // generate jwt token here
+        generateToken(newUser._id, res);
+        await newUser.save();
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            email: newUser.email,
+            profilePic: newUser.profilePic,
+        });
+    } else {
+        res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller", error.message);
+    res.status(500).json({ message: "Internl Server Error" });
+  }
 };
 
 export const login = (req, res) => {
