ce38b864bc21f6ac49677f7072fbcf8ba58689b5
Andrexxx01 <kurniawan.andre50@gmail.com>
2026-03-16 22:32:07 +0700

    setup and testing backend auth

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -3,8 +3,13 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
     try {
+        const token = req.cookies.jet;
+
+        if(!token) {
+            return res.status(401).json({ message: "Untauthorize-No Token Provided "});
+        }
 
     } catch (error) {
-        
+
     }
 }
