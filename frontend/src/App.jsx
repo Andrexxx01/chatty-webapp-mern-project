7b58d874ddd66bb87468e55952ee4537385cb29f
Andrexxx01 <kurniawan.andre50@gmail.com>
2026-03-23 22:43:00 +0700

    setup signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,37 @@
 import Navbar from "./components/navbar";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Homepage from "./pages/homepage";
 import LoginPage from "./pages/loginPage";
 import SignUpPage from "./pages/signUpPage";
 import ProfilePage from "./pages/profilePage";
 import SettingsPage from "./pages/settingsPage";
 import { useAuth } from "./store/useAuth";
+import { useEffect } from "react";
+import { Loader } from "lucide-react";
 
 const App = () => {
-  const {authUser} = useAuth();
-  
+  const {authUser, checkAuth, isCheckingAuth} = useAuth();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log({ authUser });
+
+  if(isCheckingAuth && !authUser) return(
+    <div className="flex items-center justify-center h-screen">
+      <Loader className="size-10 animate-spin" />
+    </div>
+  )
+
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/" element={authUser ? <Homepage /> : <Navigate to="/login" />} />
+        <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
+        <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
         <Route path="/settings" element={<SettingsPage />} />
       </Routes>
     </div>
--- frontend/src/store/useAuth.js
+++ frontend/src/store/useAuth.js
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
 
 export const useAuth = create((set) => ({
     authUser: null,  //initial state user 
@@ -6,4 +7,21 @@ export const useAuth = create((set) => ({
     isLoggingIn: false,
     isUpdatingProfile: false,
     isCheckingAuth: true,  //loading state, when refresh page will be check if user is aunthenthicated or not
-}))
+
+    checkAuth: async() => {
+        try {
+            const res = await axiosInstance.get("/auth/check");
+
+            set({authUser:res.data});
+        } catch (error) {
+            console.log("Error in checkAuth:", error);
+            set({authUser:null});
+        } finally {
+            set({isCheckingAuth:false});
+        }
+    },
+
+    signup: async (data) => {
+
+    }
+}));
